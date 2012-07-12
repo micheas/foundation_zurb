b6a7ccebf140c0c7aa28f2efde47ef7a3be111f7
Ishmael Sanchez <me@ishmaelsanchez.com>
2012-07-12 00:23:05 -0700

    Refine footer section and base theme CSS #1670662

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -42,53 +42,53 @@
     </div>
   </div>
 </div>
-    <div class="row">
-      <?php if ($messages): print $messages; endif; ?>
-      <?php if (!empty($page['help'])): print render($page['help']); endif; ?>
-      <div id="main" class="<?php print $main_grid; ?> columns">
-        <?php if (!empty($page['highlighted'])): ?>
-          <div class="highlight panel callout">
-            <?php print render($page['highlighted']); ?>
-          </div>
-        <?php endif; ?>
-        
-        <a id="main-content"></a>
-        <?php if ($title && !$is_front): ?>
-          <?php print render($title_prefix); ?>
-          <h1 id="page-title" class="title"><?php print $title; ?></h1>
-          <?php print render($title_suffix); ?>
-        <?php endif; ?>
-        
-        <?php if (!empty($tabs)): ?>
-            <?php print render($tabs); ?>
-            <?php if (!empty($tabs2)): print render($tabs2); endif; ?>
-        <?php endif; ?>
-        <?php if ($action_links): ?>
-          <ul class="action-links">
-            <?php print render($action_links); ?>
-          </ul>
-        <?php endif; ?>
-        <?php print render($page['content']); ?>
+<div class="row">
+  <?php if ($messages): print $messages; endif; ?>
+  <?php if (!empty($page['help'])): print render($page['help']); endif; ?>
+  <div id="main" class="<?php print $main_grid; ?> columns">
+    <?php if (!empty($page['highlighted'])): ?>
+      <div class="highlight panel callout">
+        <?php print render($page['highlighted']); ?>
       </div>
-      <?php if (!empty($page['sidebar_first'])): ?>
-        <div id="sidebar-first" class="<?php print $sidebar_first_grid; ?> columns sidebar ">
-          <?php print render($page['sidebar_first']); ?>
-        </div>
-      <?php endif; ?>
-      <?php if (!empty($page['sidebar_second'])): ?>
-        <div id="sidebar-second" class="<?php print $sidebar_sec_grid;?> columns sidebar">
-          <?php print render($page['sidebar_second']); ?>
-        </div>
-      <?php endif; ?>
+    <?php endif; ?>
+    <a id="main-content"></a>
+    <?php if ($title && !$is_front): ?>
+      <?php print render($title_prefix); ?>
+      <h1 id="page-title" class="title"><?php print $title; ?></h1>
+      <?php print render($title_suffix); ?>
+    <?php endif; ?>
+
+    <?php if (!empty($tabs)): ?>
+      <?php print render($tabs); ?>
+      <?php if (!empty($tabs2)): print render($tabs2); endif; ?>
+    <?php endif; ?>
+    <?php if ($action_links): ?>
+      <ul class="action-links">
+        <?php print render($action_links); ?>
+      </ul>
+    <?php endif; ?>
+    <?php print render($page['content']); ?>
+  </div>
+  <?php if (!empty($page['sidebar_first'])): ?>
+    <div id="sidebar-first" class="<?php print $sidebar_first_grid; ?> columns sidebar ">
+      <?php print render($page['sidebar_first']); ?>
     </div>
+  <?php endif; ?>
+  <?php if (!empty($page['sidebar_second'])): ?>
+    <div id="sidebar-second" class="<?php print $sidebar_sec_grid;?> columns sidebar">
+      <?php print render($page['sidebar_second']); ?>
+    </div>
+  <?php endif; ?>
+</div>
+<?php if ($page['footer_first'] || $page['footer_middle'] || $page['footer_last']): ?>
   <footer class="row">
     <?php if (!empty($page['footer_first'])): ?>
-      <div id="footer-first" class="five columns">
+      <div id="footer-first" class="four columns">
         <?php print render($page['footer_first']); ?>
       </div>
     <?php endif; ?>
     <?php if (!empty($page['footer_middle'])): ?>
-      <div id="footer-middle" class="three columns">
+      <div id="footer-middle" class="four columns">
         <?php print render($page['footer_middle']); ?>
       </div>
     <?php endif; ?>
@@ -98,10 +98,11 @@
       </div>
     <?php endif; ?>
   </footer>
-  <div class="bottom-bar">
-    <div class="row">
-      <div class="tweleve columns">
-        &copy; <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?>
-      </div>
+<?php endif; ?>
+<div class="bottom-bar">
+  <div class="row">
+    <div class="tweleve columns">
+      &copy; <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?>
     </div>
   </div>
+</div>
